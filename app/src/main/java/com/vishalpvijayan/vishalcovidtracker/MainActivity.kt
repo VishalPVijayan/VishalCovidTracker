2cef740981f9b0df9b52321204c19c994edf0ec2
Vishal P Vijayan <vishalpvijayan711@gmail.com>
2020-07-22 16:37:49 +0530

    Fourth Stage of Development

--- app/src/main/java/com/vishalpvijayan/vishalcovidtracker/MainActivity.kt
+++ app/src/main/java/com/vishalpvijayan/vishalcovidtracker/MainActivity.kt
@@ -2,10 +2,28 @@ package com.vishalpvijayan.vishalcovidtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchResults()
+    }
+
+    private fun fetchResults() {
+        GlobalScope.launch {
+            val response = withContext(Dispatchers.IO) { Client.api.clone().execute() }
+            if (response.isSuccessful) {
+                val data = Gson().fromJson(response.body?.string(), Response::class.java)
+                launch(Dispatchers.Main) {
+                }
+            }
+        }
     }
 }
