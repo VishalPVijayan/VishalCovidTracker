1d4f4ad72f3d76eea9f6dd22ad24a7199575aa11
Vishal P Vijayan <vishalpvijayan711@gmail.com>
2020-07-22 15:52:19 +0530

    Second Stage of Development

---
+++ app/src/main/java/com/vishalpvijayan/vishalcovidtracker/Client.kt
@@ -0,0 +1,16 @@
+package com.vishalpvijayan.vishalcovidtracker
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+object Client {
+    // create a singleton class : We can use this class with creating the instance of this class thats the benefit of using singleton class here.
+    
+    private val okHttpClient = OkHttpClient()
+
+    private val request = Request.Builder()
+        .url("https://api.covid19india.org/data.json")
+        .build()
+
+    val api = okHttpClient.newCall(request)
+}
