7566b5ccd8a87c4fd28132a82f0a4615f8d68f84
Vishal P Vijayan <vishalpvijayan711@gmail.com>
2020-07-22 15:33:49 +0530

    First Stage of Development

---
+++ app/src/main/java/com/vishalpvijayan/vishalcovidtracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vishalpvijayan.vishalcovidtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
